595b2ef9ee1b8610aebc6590a581eaf5d4091d0a
yevhenip <yevhenip@gmail.com>
2021-05-06 12:19:01 +0300

    added eventRepository

---
+++ Api/Gupy.Api/Controllers/EventsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gupy.Api.Controllers
+{
+    public class EventsController : Controller
+    {
+        public Task<IActionResult> GetAllAsync()
+        {
+            
+        }
+    }
+}
---
+++ Api/Gupy.Api/Interfaces/Repositories/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gupy.Api.Entities;
+
+namespace Gupy.Api.Interfaces.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<IEnumerable<Event>> GetAllAsync();
+        Task<Event> GetAsync(int id);
+        Task CreateAsync(Event @event);
+        Task DeleteAsync(int id);
+    }
+}
