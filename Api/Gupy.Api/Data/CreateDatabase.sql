f2d9e533a9e3d0a60f93ac522605cd8a3357e51d
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2021-05-06 11:48:25 +0300

    Init file for database

---
+++ Api/Gupy.Api/Data/CreateDatabase.sql
@@ -0,0 +1,24 @@
+﻿create schema gupy_events;
+
+create table gupy_events.events
+(
+    Id              int auto_increment
+        primary key,
+    Name            varchar(100) default 'New Event' not null,
+    Description     varchar(255) default ''          not null,
+    SubscribedCount int          default 0           null,
+    MinWantedPeople int                              null,
+    Category        varchar(100) default ''          not null,
+    Type            int                              null
+);
+
+create table gupy_events.users
+(
+    Id       int auto_increment
+        primary key,
+    Name     varchar(100) null,
+    UserName varchar(255) null,
+    Phone    varchar(100) null
+);
+
+
