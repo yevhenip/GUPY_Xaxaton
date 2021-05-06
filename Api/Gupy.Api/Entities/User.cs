8def0b9fa8e17f9ca0eb4ca0191ae04565725045
yevhenip <yevhenip@gmail.com>
2021-05-06 16:04:23 +0300

    added subscription functionality

--- Api/Gupy.Api/Entities/User.cs
+++ Api/Gupy.Api/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿namespace Gupy.Api.Entities
+﻿using System.Collections.Generic;
+
+namespace Gupy.Api.Entities
 {
     public class User
     {
@@ -6,6 +8,8 @@
         public string TelegramId { get; set; }
         public string Name { get; set; }
         public string UserName { get; set; }
+
+        public IEnumerable<UserEvent> Events { get; set; } = new List<UserEvent>();
         public string Phone { get; set; }
     }
 }
